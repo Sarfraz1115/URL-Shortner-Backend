a5fa5e165d98c00e9a093769d0c084a8dabc6846
Sarfraz1115 <syedsarfraz907@gmail.com>
2025-05-12 15:03:21 +0530

    table added

--- routes/staticRoute.js
+++ routes/staticRoute.js
@@ -1,11 +1,14 @@
 const express = require("express");
+const URL = require("../models/usermodel");
 
 const route = express.Router();
 
-route.get('/', (req, res) => {
+route.get('/', async (req, res) => {
+    const allurls = await URL.find({})
     return res.render("home", {
         id: null,
-        host: req.headers.host
+        host: req.headers.host,
+        urls: allurls,
     });
 })
 
